0ee9de381331a7039eab13553580745e9cd8a9ae
Qi Zhang <106128542+QiZhang11@users.noreply.github.com>
2023-09-07 15:36:48 +0800

    Update Figure 2B and 4B.R

--- Figure 2B and 4B.R
+++ Figure 2B and 4B.R
@@ -22,10 +22,9 @@ sample_site <- merge(sample_site, group, by ='name', all.x = TRUE)
 library(ggplot2)
 
 pcoa_plot <- ggplot(sample_site, aes(PCoA1, PCoA2, group = group1)) +
-  theme(panel.grid = element_blank(), panel.background = element_rect(color = 'black', fill = 'transparent'), legend.key = element_rect(fill = 'transparent')) + #输入绘图的要素，其实pcoa图就是xy点图，prism应该也可以做，但是颜色设置略麻烦，这边pcoa1就是x轴，pcoa2是y轴，genotype作为分组，即点的形状颜色依据。后面的是ggplot2基本主题设置，ggplot2高度可玩，可以百度搜索各种教程，这边想要罗列完是不现实的
-  geom_vline(xintercept = 0, color = 'black', size = 0.3) + #添加pco1=0的线
-  geom_hline(yintercept = 0, color = 'black', size = 0.3) + #添加pco2=0的线
-  geom_point(aes(color = group1), size = 5, alpha = 0.8) + #可在这里修改点的透明度、大小
+  theme(panel.grid = element_blank(), panel.background = element_rect(color = 'black', fill = 'transparent'), legend.key = element_rect(fill = 'transparent')) +
+  geom_hline(yintercept = 0, color = 'black', size = 0.3) + 
+  geom_point(aes(color = group1), size = 5, alpha = 0.8) + 
   labs(x = paste('PCoA axis1: ', round(100 * pcoa_eig[1], 2), '%'), y = paste('PCoA axis2: ', round(100 * pcoa_eig[2], 2), '%'))
 
 pcoa_plot
